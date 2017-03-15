bb2a5c877607668ac4333bf8f5cbc5f7924aeae1
xi-studio <110littletree@sina.com>
2017-03-15 15:29:08 +0800

    experiment 2

--- code/neuron.py
+++ code/neuron.py
@@ -1,10 +1,18 @@
 import numpy as np
 
 NEURON_NUM = 20
-W_MAX_NUM = 10 
+W_MAX_NUM  = 50 
 
-w = np.zeros((W_MAX_NUM,8)) # flag, node_head, node_tail, x_input, w, x_output
-neuron = np.zeros((NEURON_NUM,2)) # value, type
+active = np.zeros(W_MAX_NUM) 
+head   = np.zeros(W_MAX_NUM) 
+tail   = np.zeros(W_MAX_NUM) 
+w      = np.zeros(W_MAX_NUM) 
+fp_in  = np.zeros(W_MAX_NUM) 
+fp_out = np.zeros(W_MAX_NUM) 
+bp_in  = np.zeros(W_MAX_NUM) 
+bp_out = np.zeros(W_MAX_NUM) 
+nvalue = np.zeros(NEURON_NUM) 
+ntype  = np.ones(NEURON_NUM) 
         
     
 def getdata():
@@ -17,32 +25,36 @@ def getdata():
 
 
 if __name__=='__main__':
-    w[:,2] = 8
-    w[:8,1] = np.arange(8)
-    w[:8,4] = np.random.uniform(0,1,size=8)
-    data = getdata()
+    head[:8] = np.arange(8)
+    head[8:16] = np.arange(8)
+    tail[:8] = 8
+    tail[8:16] = 9
 
+    w[:16] = np.random.uniform(0,1,size=16)
+    data = getdata()
+    
     for num in np.arange(10):
 	error = []
         for (num,x) in enumerate(data[0]):
-            w[:8,3] = x
-	    w[:8,0] = 1
-            idx = np.where(w[:,0]==1)
+	    fp_in[:8] = x
+	    fp_in[8:16] = x
+	    active[:16] = 1
+            
+            idx = np.where(active==1)
+	    fp_out[idx] = fp_in[idx] * w[idx] 
 
-	    w[:,5][idx] = w[:,3][idx] * w[:,4][idx]
+	    nvalue[8] = np.sum(fp_out[:8])
+	    nvalue[9] = np.sum(fp_out[8:16])
+	    nvalue = nvalue * (nvalue>=0) 
+            nvalue[nvalue>1] = 1
 
-	    res = np.sum(w[:,5][idx])
-	    res = res * (res>=0) 
-	    if res>1:
-                res = 1
-
-	    cost= res - data[1][num]
-
-            idy = np.where(w[:,2]==8)
-	    w[:,6][idy] = cost
-	    w[:,7][idy] = w[:,6][idy] * w[:,4][idy]
-	    w[:,4][idy] = w[:,4][idy] - 0.01* w[:,3][idy] * w[:,6][idy]
-	    error.append(cost)
-        print np.sum(np.abs(np.array(error)))
+#	    cost= res - data[1][num]
+#
+#            idy = np.where(w[:,2]==8)
+#	    w[:,6][idy] = cost
+#	    w[:,7][idy] = w[:,6][idy] * w[:,4][idy]
+#	    w[:,4][idy] = w[:,4][idy] - 0.01* w[:,3][idy] * w[:,6][idy]
+#	    error.append(cost)
+            print nvalue
 
 
