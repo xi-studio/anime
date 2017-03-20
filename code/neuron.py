08d16b4d034682544b48229645568f0682fe5a3a
xi-studio <110littletree@sina.com>
2017-03-21 07:50:15 +0800

    add memory

--- code/neuron.py
+++ code/neuron.py
@@ -3,24 +3,46 @@ from scipy.sparse import csc_matrix
 
 class network(object):
   
-    def __init__(self):
-        NEURON_NUM = 10000
-        W_MAX_NUM  = 100000 
+    def __init__(self, wmatrix=None, nmatrix=None, nnum=1000, wnum=5000, trainable=False):
+        NEURON_NUM = nnum
+        W_MAX_NUM  = wnum 
             
-        self.head    = csc_matrix((W_MAX_NUM,NEURON_NUM),dtype=np.int8)
-        self.tail    = csc_matrix((W_MAX_NUM,NEURON_NUM),dtype=np.int8) 
         self.w       = np.zeros(W_MAX_NUM,dtype=np.float32) 
         self.nvalue  = np.zeros(NEURON_NUM,dtype=np.float32)
         self.nsymbol = np.ones(NEURON_NUM,dtype=np.int8) 
+        self.nmemory = np.zeros(NEURON_NUM,dtype=np.float32)
+
+        if wmatrix==None:
+            self.head    = csc_matrix((W_MAX_NUM,NEURON_NUM),dtype=np.int8)
+            self.tail    = csc_matrix((W_MAX_NUM,NEURON_NUM),dtype=np.int8) 
+        else:
+            length = wmatrix.shape[0]
+            row = np.arange(lenth)
+            data = np.ones(length)
+            col_head = wmatrix[:,0]
+            col_tail = wmatrix[:,1]
             
+            assert np.max(col_head) <= nnum
+            assert np.max(col_tail) <= nnum
+
+            self.head    = csc_matrix((data,(row,col_head)),shape=(W_MAX_NUM,NEURON_NUM),dtype=np.int8)
+            self.tail    = csc_matrix((data,(row,col_tail)),shape=(W_MAX_NUM,NEURON_NUM),dtype=np.int8) 
+            self.w[:length] = wmatrix[:,2]
+
+        if nmatrix!=None:
+            length = nmatrix.shape[0]
+            self.nsymbol[:length] = nmatrix
+          
+        self.trainable = trainable    
         
     def step(self):
         active_w = self.head.dot(self.nvalue * self.nsymbol)
         output = (self.tail.T).dot(active_w)
-        output = output * (output>=0)
+        active_n = output * ((output - self.nmemory)>=0)
 
-        lr = 0.01
-        self.w = self.w + lr * self.tail.dot(output) * active_w
+        if self.trainable:
+           lr = 0.01
+           self.w = self.w + lr * self.tail.dot(output) * active_w
         
         
     
@@ -36,7 +58,7 @@ def getdata():
     
 if __name__=='__main__':
     n = network()
-    for x in range(1000):
+    for x in range(100):
         n.step()
 
 
