b0eaa7d9f20ecdda405162924c826616719ab8c6
xi-studio <110littletree@sina.com>
2017-03-20 08:53:08 +0800

    scipyt sparse

--- code/neuron.py
+++ code/neuron.py
@@ -1,22 +1,27 @@
 import numpy as np
+from scipy.sparse import csc_matrix
 
 class network(object):
   
     def __init__(self):
-        NEURON_NUM = 1000
-        W_MAX_NUM  = 2000 
+        NEURON_NUM = 10000
+        W_MAX_NUM  = 100000 
             
-        self.head    = np.zeros((W_MAX_NUM, NEURON_NUM),dtype=np.int8)
-        self.tail    = np.zeros((W_MAX_NUM, NEURON_NUM),dtype=np.int8) 
+        self.head    = csc_matrix((W_MAX_NUM,NEURON_NUM),dtype=np.int8)
+        self.tail    = csc_matrix((W_MAX_NUM,NEURON_NUM),dtype=np.int8) 
         self.w       = np.zeros(W_MAX_NUM,dtype=np.float32) 
         self.nvalue  = np.zeros(NEURON_NUM,dtype=np.float32)
         self.nsymbol = np.ones(NEURON_NUM,dtype=np.int8) 
             
         
     def step(self):
-        res = np.dot(np.dot(self.head,self.nvalue * self.nsymbol) * self.w, self.tail)
-        res = res * (res>=0)
-        print np.sum(res)
+        active_w = self.head.dot(self.nvalue * self.nsymbol)
+        output = (self.tail.T).dot(active_w)
+        output = output * (output>=0)
+
+        lr = 0.01
+        self.w = self.w + lr * self.tail.dot(output) * active_w
+        
         
     
 def getdata():
@@ -27,9 +32,11 @@ def getdata():
     data = (x,y)
     return data    
 
+    
+    
 if __name__=='__main__':
     n = network()
-    for x in range(100):
+    for x in range(1000):
         n.step()
 
 
