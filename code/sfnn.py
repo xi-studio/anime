16d4d86b29e9cbfbf3889358d7fe775e74b0c61d
xi-studio <110littletree@sina.com>
2017-04-21 16:46:30 +0800

    something

--- code/sfnn.py
+++ code/sfnn.py
@@ -10,7 +10,7 @@ class network(object):
 	    self.w = w    
         else:
 	    self.w = csc_matrix((nnum,nnum),dtype=np.float32) 
-        self.v = csc_matrix((nnum,1),dtype=np.float32)
+        self.v = csc_matrix((1,nnum),dtype=np.float32)
 	self.trainable = trainable
 
     def run(self,times=10):
@@ -24,15 +24,16 @@ class network(object):
 	        self.step_predict()
     
     def step_predict(self):
-        res = self.w.dot(self.v)  
+        res = self.v.dot(self.w)  
         self.v = res.multiply(res > 0)
+
 	print np.sum(self.v)
 
     def step_train(self):
         v_stash = self.v
-        res = self.w.dot(self.v)  
-        self.v = res.multiply(res > 0)
-        self.w = self.w - 0.01 * v_stash * self.v.T
+        res = self.v.dot(self.w)  
+        self.v = res.multiply(res > 0.1)
+        #self.w = self.w + 0.01 * v_stash.T * self.v
 	print np.sum(self.v)
 
 def getdata():
@@ -47,12 +48,16 @@ def getdata():
 if __name__=='__main__':
     BA = nx.random_graphs.barabasi_albert_graph(1000, 1)
     idx = np.array(BA.edges())
-    data = np.random.uniform(0,1,idx.shape[0])
+    #data = np.random.uniform(0,1,idx.shape[0])
+    data = np.ones(idx.shape[0])
     w = csc_matrix((data, (idx[:,0], idx[:,1])), shape=(1000,1000))
 
+    print np.sum(w,axis=1)
+
     n = network(w=w)
-    n.v[:200] = np.random.uniform(0,10,(200,1))
-    #n.trainable = True
-    n.run(times=10)
+    #n.v[:,:20] = np.random.uniform(0,1,20)
+    n.v[:,:20] = np.ones(20)
+    n.trainable = True
+    n.run(times=20)
 
 
