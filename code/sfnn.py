21e14e32ee91fb071e3cb2dbe104a245cd622d0c
xi-studio <110littletree@sina.com>
2017-05-04 18:53:07 +0800

    something

--- code/sfnn.py
+++ code/sfnn.py
@@ -1,16 +1,18 @@
 import numpy as np
-from scipy.sparse import csc_matrix
-from sklearn.preprocessing import normalize
 import cPickle
 import gzip
+from scipy.sparse import csc_matrix
+from sklearn.preprocessing import normalize
 
+from profilehooks import profile
 import networkx as nx
 import matplotlib.pyplot as plt
+
 
 class network(object):
     def __init__(self, nsize=1000, w=None, b=0, lr=0):
         
-        self.nsize = 1000
+        self.nsize = nsize
 
         if w!=None:
 	    self.w = w    
@@ -19,34 +21,34 @@ class network(object):
 
         self.b  = b
         self.lr = lr
-        self.v  = csc_matrix((1,nsize),dtype=np.float32)
+        self.v  = np.zeros(nsize,dtype=np.float32)
+	self.idx = self.w.nonzero()
 
     def run(self,times=10):
-        base = np.zeros(1000*(times+1))
-        base[:1000] = self.v.toarray()
         for n in range(times):
            if self.lr == 0:
 	       self.step_predict()
-	       base[1000*(n+1):1000*(n+2)] = self.v.toarray()
 	   else:
 	       self.step_train()
 
            #print 'lr:',self.lr
 	   #print self.v.sum()
 	   #show((self.v.toarray())[0],3)
-        return base
     
     def step_predict(self):
-        res    = self.v.dot(self.w)  
-        self.v = res.multiply(res > self.b)
+        res    = self.w.dot(self.v) 
+        self.v = res * (res > self.b)
 
     def step_train(self):
-        v_stash = self.v
-        res     = self.v.dot(self.w)  
-        self.v  = res.multiply(res > self.b)
+	before = np.searchsorted(np.where(self.v>0)[0],self.idx[0])>0
 
-        self.w  = self.w + self.w.multiply(v_stash.T>0).multiply(self.v>0) * self.lr
-        self.w  = normalize(self.w, norm='l1', axis=1)
+        res    = self.w.dot(self.v) 
+        self.v = res * (res > self.b)
+
+	after  = np.searchsorted(np.where(self.v>0)[0],self.idx[1])>0 
+
+	self.w.data += self.w.data * before * after
+        self.w  = normalize(self.w, norm='l1', axis=0)
 
     def save_w(self,name):
 	with open(name, 'wb') as f:
@@ -71,8 +73,8 @@ def graph():
     idx = np.array(BA.edges())
     data = np.random.uniform(0,1,idx.shape[0])
     #data = np.ones(idx.shape[0])
-    w = csc_matrix((data, (idx[:,0], idx[:,1])), shape=(1000,1000))
-    w = normalize(w, norm='l1', axis=1)
+    w = csc_matrix((data, (idx[:,1], idx[:,0])), shape=(1000,1000))
+    w = normalize(w, norm='l1', axis=0)
 
     return w
 
@@ -102,24 +104,12 @@ def load_data(num):
         data_set = cPickle.load(f)
     return  data_set[0][0][:num],data_set[0][1][:num]
 
-    
-if __name__=='__main__': 
+def test():
     data,y = load_data(3000)
     filename = '../data/weight/mnist_size_3000.pkl'
     w = graph() 
-    n = network(w=w, b=0.1, lr=1)
-#    n = network(b=0.1,lr=0)
-#    n.load_w(filename)
+    n = network(w=w, b=0.1, lr=0)
 
-#    data = getdata()
-#    
-#    for x in range(50):
-#        #n.v[:,:700] = np.random.randint(low=0,high=2,size=700)
-#        n.v[:,:700] = np.ones(700)
-#	n.run(times=10)
-#	a = n.w.sum(axis=0)
-#	show(a.T,2)
-#    w = n.w
     base = np.zeros((data.shape[0],1000*5))
     for epoch in range(30):
         res = 0
@@ -141,6 +131,25 @@ if __name__=='__main__':
 #        #show(n.w.toarray()[:,100],0.3)
 #
     n.save_w(filename)
+
+@profile
+def test_speed():
+    filename = '../data/weight/work1.pkl'
+    w = graph() 
+    n = network(w=w, b=0.1, lr=1)
+#    n = network(b=0.1,lr=0)
+#    n.load_w(filename)
+
+    pic = np.ones(700)
+    for x in range(1000):
+        n.v[:700] = pic 
+	n.run(times=5)
+	#print n.w.sum()
+#    n.save_w(filename)
+    
+    
+if __name__=='__main__': 
+    test_speed()
     
 
 
