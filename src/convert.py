66056d6c0f354c6ba3716dcb3eff0a36ed778452
xi-studio <110littletree@sina.com>
2017-05-14 08:53:11 +0800

    add data

--- src/convert.py
+++ src/convert.py
@@ -1,63 +1,69 @@
 from __future__ import division
+import midi
 import numpy as np
 import cPickle
 import gzip
-import midi
 import glob
 
-from midi.utils import midiread, midiwrite
 
-r=(21, 109)
-dt=0.3
-scale = 400
-scale_1d = scale
+SCALE = 100
+PILEN = 1+127+1+1
+
+def Note2array(onoff, pitch , velocity , tick ):
+    note = np.zeros(PILEN)
+    note[0] = onoff
+    note[pitch+1] = 1 
+    note[-2] = velocity/127.0
+    note[-1] = tick/960.0
+
+    return  note
 
 
-def align(data):
-    res = []
-    for (x,y) in data:
-        if len(x) < scale_1d:
-            base = np.zeros(2*scale_1d)
-            base[ : len(x)] = x / (r[1]-r[0])
-            base[scale_1d : len(y)+scale_1d] = y / scale_1d
-            res.append(base)
-        else:
-            num = len(x) - scale_1d
-            for idx in range(num):
-                base = np.zeros(2*scale_1d)
-                base[ : scale_1d] = x[idx : idx + scale_1d] / (r[1]-r[0])
-                base[scale_1d : ] = y[idx : idx + scale_1d] / scale_1d
-                res.append(base)
-        
-    print len(res)
-    return np.array(res)
-        
+def midi2keystrikes(filename,tracknum=0):
+    p = midi.read_midifile(filename)
+    events = p[tracknum]
+    result = []
     
+    for e in events:
+        if isinstance(e,midi.NoteOnEvent):
+            res = Note2array(1, e.pitch, e.velocity, e.tick)
+            result.append(res)
+        if isinstance(e,midi.NoteOffEvent):
+            res = Note2array(0, e.pitch, e.velocity, e.tick)
+            result.append(res)
+            
+    if (len(result) == 0) and (tracknum <5):
+        # if it didn't work, scan another track.
+        return midi2keystrikes(filename,tracknum+1)
+        
+    return np.array(result)   
+
+
 def miditomatrix(dataset): 
     files = glob.glob(dataset)
+    num = 0
     base = []
     for f in files:
-        print f
-        data = midiread(f, r, dt).piano_roll
-        if data.shape[0] <= scale:
-            res = np.where(data>0)
-            base.append(res)
-        else:
-            num = data.shape[0] - scale
-            for idx in range(num):
-                pic = data[idx : idx + scale]
-                res = np.where(pic>0)
-                base.append(res)
+        track = midi2keystrikes(f)
+        size = int(np.ceil(track.shape[0]/SCALE))
+        mod = track.shape[0] % SCALE
+        if mod!=0:
+            track = np.append(track,np.zeros((SCALE - mod,PILEN)),axis=0)
 
-    res = align(base)
+        for n in range(size):
+            base.append(track[SCALE*n:SCALE*(n+1),:])
+    
+        print len(base)
+       
+        num = num + 1
+       
+    res = 1
+    print num
     return res
 
 
 if __name__=="__main__":
-    res = miditomatrix('../data/data_set/*.mid')
-    with gzip.open('../data/400scale_seq.pkl.gz','wb') as f:
-        cPickle.dump(res,f)
-        print "save res"
+    res = miditomatrix('../data/midi_set/*.mid')
 
     
     
