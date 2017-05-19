442d11da5e6084f310c39541e19019477557ddf8
xi-studio <110littletree@sina.com>
2017-05-19 11:59:43 +0800

    conver midi

--- src/convert_midi.py
+++ src/convert_midi.py
@@ -5,14 +5,16 @@ import cPickle
 import gzip
 import glob
 
+import matplotlib.pyplot as plt
+
 tmp = 960    # ticks per beat
 ts  = tmp/16 # time scale
-ps  = 2      # picth scale
-sc  = 10     # 10 seconds
+ps  = 3      # picth scale
+sc  = 5     # 10 seconds
 
 size = (16*sc, (127//ps)*2)
 
-print size
+print 'Pic size',size
 
 def midi2keystrikes(filename,tracknum=0):
     p = midi.read_midifile(filename)
@@ -32,11 +34,13 @@ def midi2keystrikes(filename,tracknum=0):
             
     if (len(result) == 0) and (tracknum <5):
         return midi2keystrikes(filename,tracknum+1)
-        
-    base = np.zeros((int(np.ceil(abs_time / ts)),(127//ps)*2))
+
+    timesize = result[-1][0]    
+    timesize = np.ceil(timesize / (16*sc)) * 16 *sc
+    base = np.zeros((int(timesize),(127//ps)*2))
     idx = np.array(result)   
     base[idx[:,0],idx[:,1]] = 1
-    print base
+    base = base.reshape((-1,80*(127//ps)*2))
     return base 
 
 
@@ -46,31 +50,24 @@ def miditomatrix(dataset):
     base = []
     for f in files:
         track = midi2keystrikes(f)
-        break
- 
-#        size = int(np.ceil(track.shape[0]/SCALE))
-#        mod = track.shape[0] % SCALE
-#        if mod!=0:
-#            track = np.append(track,np.zeros((SCALE - mod,PILEN)),axis=0)
-#
-#        for n in range(size):
-#            pic = track[SCALE*n:SCALE*(n+1),:]
-#            base.append(pic.reshape(SCALE * SCALE))
-#    
-#        num = num + 1
-#       
-#    print num
-#    print len(base)
+
+        for x in track:
+            base.append(x)
+    
+        num = num + 1
+       
+    print num
+    print len(base)
     return np.array(base)
 
 
 if __name__=="__main__":
     data = miditomatrix('../data/midi_set/*.mid')
-#    data = data[:-(data.shape[0]%100)]
-#    ta = np.ones(data.shape[0])
-#    res = (data,ta)
-#    with gzip.open("../data/midi.pkl.gz","wb") as f:
-#        cPickle.dump(res,f)
-#
-#    
-#    
+    data = data[:-(data.shape[0]%100)]
+    ta = np.ones(data.shape[0])
+    res = (data,ta)
+    with gzip.open("../data/midi.pkl.gz","wb") as f:
+        cPickle.dump(res,f)
+
+    
+    
