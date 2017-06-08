952ac1f3bcf71a91c19ba614fb282b083c383167
xi-studio <110littletree@sina.com>
2017-06-08 17:31:23 +0800

    mlp

--- newnet/sfnn_mlp.py
+++ newnet/sfnn_mlp.py
@@ -18,6 +18,43 @@ class sfnn_layer(object):
         self.g = np.ones(num)
         self.lr = 0.1
         
+
+def load_data():
+    filepath = '../data/mnist.pkl.gz'
+    f = gzip.open(filepath)
+    train_data,test_data,valid_data = cPickle.load(f)
+    x_data,y_data = train_data
+    return x_data[:500],y_data[:500]
+
+if __name__ =='__main__':
+    x_data,idy = load_data()
+    y_data = np.zeros((500,10))
+    y_data[np.arange(500),idy] = 1
+
+    w = np.random.uniform(0,1,(784,10))
+    w = normalize(w, norm='l1', axis=1)
+
+    g = np.ones(10)
+
+
+    for iter in range(100):
+        epo_sum = 0
+        for num,x in enumerate(x_data):
+            res = np.dot(x,w)
+       	    res = res*(res>=0) 
+            w = w -  0.01*x[:,None] * w * (res>0) * g 
+	   # w = w*(w>0)
+            g = res - y_data[num]
+           # w = normalize(w, norm='l1', axis=1)
+            epo_sum += np.sum(np.abs(g))
+	print epo_sum
+    
+
+
+
+    
+
+
     
 
 
